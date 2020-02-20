ed0713a41e13fc7eefd843079825d3add1ccb41c
ahmed daoud <ahmed-daoud1@outlook.fr>
2020-02-20 15:15:32 +0100

    add first project

---
+++ GestionPro/GestionPro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
